198b54af1ccda1c48a9ebafd1b6ea814aeda9c79
Robin DIDIER <rdidier@e3r6p19.42.fr>
2016-04-06 12:36:22 +0200

    exercice 03

---
+++ d01/ex03/main.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?php
+include("ft_split.php");
+
+print_r(ft_split("Hello        World 42  "));
+print_r(ft_split("     Hello   "));
+print_r(ft_split("     Hello"));
+print_r(ft_split("Hello   "));
+print_r(ft_split("Hello"));
+print_r(ft_split(" H e l l o "));
+print_r(ft_split("    H   e    l l o       d "));
+
+
+?>
