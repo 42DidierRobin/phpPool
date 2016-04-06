18ee695275350ba250cf2c0db1379861f5fad67b
Robin DIDIER <rdidier@e3r6p19.42.fr>
2016-04-06 13:52:11 +0200

    up et ex06

---
+++ d01/ex06/ssap.php
@@ -0,0 +1,58 @@
+#!/usr/bin/php
+<?php
+function ft_split($string)
+{
+    $pos = 1;
+    $new_str = "";
+    $tab = [];
+    $len = strlen($string);
+
+    while (substr($string, 0, 1) == " ")
+    {
+        $string = substr($string, 1, $len - 1);
+        $len--;
+    }
+    while ($pos = strpos($string, ' '))
+    {
+        $new_str =  substr($string, 0, $pos);
+        $len = strlen($string);
+        $string = substr($string, $pos, $len);
+        while (substr($string, 0, 1) == " ")
+        {
+            $string = substr($string, 1, $len - 1);
+            $len--;
+        }
+        array_push($tab, $new_str);
+    }
+    if(strlen($string))
+        array_push($tab, $string);
+    sort($tab);
+    return ($tab);
+}
+
+$i = 1;
+$j = 0;
+$res = [];
+$tmp = [];
+
+while ($argv[$i]) {
+    $tmp = ft_split($argv[$i]);
+    while ($tmp[$j]) 
+    {
+        array_push($res, $tmp[$j]);
+        $j++;
+    }
+    $j = 0;
+    $i++;
+}
+sort($res);
+$i = 0;
+if ($argc > 1)
+{
+    while ($res[$i]) 
+    {
+        echo $res[$i] . "\n";
+        $i++;
+    }
+}
+?>
