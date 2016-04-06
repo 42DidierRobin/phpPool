b3b57510bd6b6b6f4397ddf330ff1af1336d7cd6
Robin DIDIER <rdidier@e3r6p19.42.fr>
2016-04-06 20:35:41 +0200

    Ex12

--- d01/ex12/search_it!.php
+++ d01/ex12/search_it!.php
@@ -1,10 +1,36 @@
 #!/usr/bin/php
 <?php
 
-if ($argc > 3)
+if ($argc > 2)
 {
-   foreach($argv as $k => $v) {
-       echo $k." -> ".$v."\n";
-   }
+    $key = $argv[1];
+ 
+    foreach ($argv as $k => $v)
+    {
+        if ($k > 1)
+        {
+            $i = 0;
+            
+            $k_tmp = "";
+            $v_tmp = "";
+            while ($v[$i])
+            {
+                if ($v[$i] == ':')
+                {
+                    $i++;
+                    while ($v[$i])
+                    {
+                        $v_tmp = $v_tmp.$v[$i];
+                        $i++;
+                    }
+                    break;
+                }
+                $k_tmp = $k_tmp.$v[$i];
+                $i++;
+            }
+            if ($k_tmp == $key)
+                echo $v_tmp."\n";
+        }
+    }
 }
 ?>
