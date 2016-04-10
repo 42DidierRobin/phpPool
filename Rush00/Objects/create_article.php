06071794db465683aecc034f13fcaffc3cef18ae
Robin DIDIER <rdidier@e3r4p22.42.fr>
2016-04-10 12:52:09 +0200

    on se lance dans lapi

--- Rush00/Objects/create_article.php
+++ Rush00/Objects/create_article.php
@@ -1,33 +1,25 @@
 <?PHP
-include("handle_data.php");
+    include("handle_data.php");
 
-if ($_POST["id"] && $_POST["price"])
-{
-	$article = ft_get_data("article");
-	$tab["id"] = $_POST["id"];
-	$tab["price"] = $_POST["price"];
-	$tab["img"] = $_POST["img"];
-	$tab["class"] = $_POST["class"];
-	$article[] = $tab;
-	ft_save_data("article", $article);
-}
+    function new_article($id, $name, $platform, $genre, $img, $resume)
+    {
+        $content = ft_get_data("../data/article");
+        if (!$content)
+            $content = array();
+        $article['id'] = $id;
+        $article['name'] = $name;
+        $article['resume'] = $resume;
+        $article['platform'] = array();
+        foreach ($platform as $k => $v)
+            array_push($article['platform'], $v);
+        $article['img'] = array();
+        foreach ($img as $k => $v)
+            array_push($article['img'], $v);
+        $article['genre'] = array();
+        foreach ($genre as $k => $v)
+            array_push($article['genre'], $v);
+        array_push($content, $article);
+        ft_save_data("../data/article", $article);
+    }
+
 ?>
-
-<meta charset="UTF-8">
-<html>
-  <head>
-  </head>
-  <body>
-	<form action="create_article.php" method="post">
-	  Designation:<br/>
-	  <input type="text" name="id"><br/><br/>
-	  Prix:<br/>
-	  <input type="number" name="price"><br/><br/>
-	  Image:<br/>
-	  <input type="url" name="img"><br/><br/>
-	  Categories:<br/>
-	  <?php include("class_radio.php");?><br/>
-	  <input type="submit" name="submit" value="OK">
-	</form>
-  </body>
-</html>
