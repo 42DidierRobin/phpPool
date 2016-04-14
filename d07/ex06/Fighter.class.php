ae5357730d7bc3d49baf32c8b23d2bead52eb084
Robin DIDIER <rdidier@e3r4p22.42.fr>
2016-04-14 19:16:54 +0200

    frwege

--- d07/ex06/Fighter.class.php
+++ d07/ex06/Fighter.class.php
@@ -1,2 +1,23 @@
 <?php
-    
+
+    abstract class Fighter
+    {
+        private $name;
+
+        public function __construct($name)
+        {
+            $this->name = $name;
+        }
+
+        public abstract function fight($target);
+
+        public function getName()
+        {
+            return $this->name;
+        }
+        
+        public function __toString()
+        {
+            return ($this->name);
+        }
+    }
--- d07/ex06/UnholyFactory.class.php
+++ d07/ex06/UnholyFactory.class.php
@@ -1,2 +1,55 @@
 <?php
-    
+
+    class UnholyFactory
+    {
+
+        private $list_fighter;
+
+        public function __construct()
+        {
+            $this->list_fighter = array();
+        }
+
+        public function absorb($a)
+        {
+            if ($a instanceof Fighter)
+            {
+                $add = true;
+                foreach ($this->list_fighter as $k => $v)
+                    if ($v == $a)
+                        $add = false;
+                if ($add)
+                {
+                    array_push($this->list_fighter, $a);
+                    echo "(Factory absorbed a fighter of type ".$a.")\n";
+                } else
+                    echo "(Factory already absorbed a fighter of type ".$a.")\n";
+            } else
+                echo "(Factory can't absorb this, it's not a fighter)\n";
+        }
+
+        public function fabricate($requested)
+        {
+            $bool;
+            foreach ($this->list_fighter as $k => $v)
+            {
+                if ($v->getName() == $requested)
+                {
+                    echo "(Factory fabricates a fighter of type ".$v."\n";
+                    $bool = $v;
+                    return clone $v;
+                } else
+                {
+                    echo "(Factory hasn't absorbed any fighter of type ".$v."\n";
+                    $bool = false;
+                    return (null);
+                }
+            }
+            if ($bool)
+                return clone $bool;
+            else
+                return (null);
+
+        }
+
+    }
